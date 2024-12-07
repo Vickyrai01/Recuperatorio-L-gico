bf3f716ad72da5b2ad1acf2001c4b69e4a36bd7f
Vicky <133067917+Vickyrai01@users.noreply.github.com>
2024-12-07 09:44:18 -0300

    agrego base de conociemientos

---
+++ prologpiedades.pl
@@ -0,0 +1,18 @@
+/*
+funtores:
+    casa(metrosCuadrados)
+    departamento(cantAmbientes)
+    loft(añoConstruccion)
+
+
+*/
+
+%vive(Persona, Propiedad, Barrio).
+vive(juan, casa(120), almagro).
+vive(nico, departamento(3,2), almagro).
+vive(alf, departamento(3,1), almagro).
+vive(julian, loft(2000), almagro).
+vive(vale, departamento(4,1), flores).
+vive(fer, casa(110), flores).
+
+vive(vicky, departamento(4,2), lanus).
